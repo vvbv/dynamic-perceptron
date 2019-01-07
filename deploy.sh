c74fbf3e4e0b2b57d829e11913241b68f4b82c7b
vvbv <jeison.cardona@correounivalle.edu.co>
2019-01-07 03:08:54 -0500

    deploy script fixed

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,9 @@ cp -r ../$1/neurons.conf ../deploy_area/neurons.conf
 cp -r ../$1/structure.conf ../deploy_area/structure.conf
 cp -r ../$1/input.txt ../deploy_area/input.txt
 
+cp include/neuron.hpp include/base_neuron.hpp
+cp src/neuron.cpp src/base_neuron.cpp
+
 NEURAL_NETWORK_STRUCTURE_FILE='structure.conf'
 filename="neurons.conf"
 
@@ -41,15 +44,23 @@ do
                     WEIGHT=$tok
                 fi
             done
+
+            rm include/neuron.hpp
+            rm src/neuron.cpp
+
+            cp include/base_neuron.hpp include/neuron.hpp
+            cp src/base_neuron.cpp src/neuron.cpp
             
             sed -i -e 's/{NEURON_CODENAME}/'$NEURON_CODENAME'/g' include/neuron.hpp
             sed -i -e 's/{NEURAL_NETWORK_STRUCTURE_FILE}/'$NEURAL_NETWORK_STRUCTURE_FILE'/g' include/neuron.hpp
             sed -i -e 's/this->weight = 0.5;/this->weight = '$WEIGHT';/g' src/neuron.cpp
             sed -i -e 's/this->layer_number = 0;/this->layer_number = '$LAYER_NUMBER';/g' src/neuron.cpp
-            g++ -g --std=c++11 src/*.cpp -I include/ -o build/$NEURON_CODENAME
+            g++ -g --std=c++11 src/main.cpp src/neuron.cpp -I include/ -o build/$NEURON_CODENAME
             echo "[ Build completed ]:    Neuron:"$NEURON_CODENAME "[Weight:"$WEIGHT", Layer:"$LAYER_NUMBER"]"
 
         fi
     fi
 
 done < "$filename"
+rm include/base_neuron.hpp
+rm src/base_neuron.cpp
